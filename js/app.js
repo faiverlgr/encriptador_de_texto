4fc062647f8b0b70d08c81ef46d17bdef796c722
faiverlgr <faiverlgr@hotmail.com>
2024-07-11 18:31:51 -0500

    agrega mensaje error escritura

--- js/app.js
+++ js/app.js
@@ -12,9 +12,12 @@ tambien devolver una palabra encriptada para su version original
 d
 */
 
+// valores predefinidos
+document.getElementById('con__mensaje').style.display = 'none';
+document.getElementById('boton__copiado').style .display = 'none';
+document.getElementById('sin__mensaje').style .display = 'block';
 
 //variables
-
 var listaVocales = new Array(2)
 listaVocales[0] = new Array(2)
 listaVocales[1] = new Array(2)
@@ -33,85 +36,7 @@ listaVocales[3][1]="ober"
 listaVocales[4][0]="u"    
 listaVocales[4][1]="ufat"
 
-
-function char(parChar){
-    var alfabeto = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
-    var rpta = false
-
-    for (var f=0; f < alfabeto.length; f++) {
-        if (alfabeto[f] == parChar) {
-            rpta = true
-            break
-        }
-    }
-    return rpta
-}
-
-function desencriptar(parTextoSeguro){
-    let arrPalabras = parTextoSeguro.split(" ");
-    let nuevaCadena = ""
-    let matrizCoincidencias
-    //const patron = new RegExp(/(ai)|(enter)|(imes)|(ober)|(ufat)/g)
-    let patron = /ai|enter|imes|ober|ufat/g;
-    //let cadena = "gaitober"
-    //console.log(cadena.match(patron));
-    //console.log(patron.exec(cadena));
-
-    for (var f=0; f < arrPalabras.length; f++) {
-        cadena = arrPalabras[f]
-        matrizCoincidencias = cadena.match(patron)
-        nuevaCadena = cadena
-        
-        if(matrizCoincidencias != null){
-            for (x = 0; x < matrizCoincidencias.length; x++){
-                //reemplazando
-                for(var j=0; j<listaVocales.length; j++){          
-                    //si la letra es una vocal la almacena
-                    if (matrizCoincidencias[x] == listaVocales[j][1]){
-                        nuevaCadena = cadena.replace(matrizCoincidencias[x], listaVocales[j][0])
-                        cadena = nuevaCadena
-                        //console.log(nuevaCadena)
-                        break
-                    }
-                }
-            }
-        }
-        //reemplaza palabra en matriz
-        arrPalabras[f] = nuevaCadena
-    }
-    // arma frase con nuevas palabras
-    nuevaCadena = ""
-    for (var f=0; f < arrPalabras.length; f++) {
-        nuevaCadena = nuevaCadena + arrPalabras[f] + " "
-    }
-    return nuevaCadena
-}
-
-
-function encriptar(parTextoOriginal){
-    var cadena = ""
-    var vocalAlmacenada = 0
-
-    for (var f=0; f < parTextoOriginal.length; f++) {
-        vocalAlmacenada=0
-        for(var j=0; j<listaVocales.length; j++){          
-            //si la letra es una vocal la almacena
-            if (parTextoOriginal[f] == listaVocales[j][0]){
-                vocalAlmacenada = j+1
-                break
-            }            
-        }       
-
-        if (vocalAlmacenada > 0) {            
-            cadena = cadena + listaVocales[j][1]
-        }else{
-            cadena = cadena + parTextoOriginal[f]            
-        }
-    }
-    return cadena
-}
-
-
+//funcion que hace primeras validaciones del texto recibido
 function recibeTextoElemento(parBoton){
     let parTexto = document.getElementById('texto__escrito').value;
     console.log("textoRecibido: " + parTexto)
@@ -134,22 +59,20 @@ function recibeTextoElemento(parBoton){
         }        
     }
     
+    // valida el texto escrito
     if (!esValido){
         document.getElementById('sin__mensaje').style.display = "block";
         document.getElementById('con__mensaje').style.display = "none";
         document.getElementById('boton__copiado').style .display = 'none';
-
-        console.log("Revise el texto. Contiene caracteres no validos: " + parTexto)
+        alert('Revise el texto. Contiene caracteres no validos.')
+        //console.log("Revise el texto. Contiene caracteres no validos: " + parTexto)
     }else{
         if (parBoton == 1){
             var textoSeguro = encriptar(parTexto)
-            //console.log("el texto devuelto es: " + textoSeguro)
-            //var x = document.getElementById("con_mensaje");
-            //x.style.display = "hidden"    
+   
         }else{
             var textoSeguro = desencriptar(parTexto)
-            
-            //console.log("el texto devuelto es: " + textoSeguro)
+ 
         }
         document.getElementById('sin__mensaje').style.display = 'none';
         document.getElementById('con__mensaje').style.display = 'block';
@@ -161,16 +84,86 @@ function recibeTextoElemento(parBoton){
     }    
     return esValido
 }
-document.getElementById('con__mensaje').style.display = 'none';
-document.getElementById('boton__copiado').style .display = 'none';
-document.getElementById('sin__mensaje').style .display = 'block';
 
-/*
-document.getElementById('sin__mensaje').style .display = 'none';
-*/
-//console.log(recibeTextoElemento("elsa"));
-//console.log(char("d"));
 
+// funcion para validar si el caracter es valido
+function char(parChar){
+    var alfabeto = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+    var rpta = false
+
+    for (var f=0; f < alfabeto.length; f++) {
+        if (alfabeto[f] == parChar) {
+            rpta = true
+            break
+        }
+    }
+    return rpta
+}
+
+//funcion para desencriptar la cadena
+function desencriptar(parTextoSeguro){
+    let arrPalabras = parTextoSeguro.split(" ");
+    let nuevaCadena = ""
+    let matrizCoincidencias
+    let patron = /ai|enter|imes|ober|ufat/g;
+
+    //recorre cada palabra de la frase
+    for (var f=0; f < arrPalabras.length; f++) {
+        cadena = arrPalabras[f]
+        matrizCoincidencias = cadena.match(patron)
+        nuevaCadena = cadena
+        
+        if(matrizCoincidencias != null){
+            for (x = 0; x < matrizCoincidencias.length; x++){
+                //reemplazando las letras
+                for(var j=0; j<listaVocales.length; j++){          
+                    //si la letra es una vocal la almacena
+                    if (matrizCoincidencias[x] == listaVocales[j][1]){
+                        nuevaCadena = cadena.replace(matrizCoincidencias[x], listaVocales[j][0])
+                        cadena = nuevaCadena
+                        break
+                    }
+                }
+            }
+        }
+        //reemplaza nueva palabra en matriz
+        arrPalabras[f] = nuevaCadena
+    }
+    // arma frase con nuevas palabras
+    nuevaCadena = ""
+    for (var f=0; f < arrPalabras.length; f++) {
+        nuevaCadena = nuevaCadena + arrPalabras[f] + " "
+    }
+    return nuevaCadena
+}
+
+//funcion para encriptar el texto
+function encriptar(parTextoOriginal){
+    var cadena = ""
+    var vocalAlmacenada = 0
+
+    //recorre cada letra del texto recibido
+    for (var f=0; f < parTextoOriginal.length; f++) {
+        vocalAlmacenada=0
+        // busca el caracter en la funcion de validacion de caracteres
+        for(var j=0; j<listaVocales.length; j++){          
+            //si la letra es una vocal la almacena
+            if (parTextoOriginal[f] == listaVocales[j][0]){
+                vocalAlmacenada = j+1
+                break
+            }            
+        }       
+        //arma cadena encriptada
+        if (vocalAlmacenada > 0) {            
+            cadena = cadena + listaVocales[j][1]
+        }else{
+            cadena = cadena + parTextoOriginal[f]            
+        }
+    }
+    return cadena
+}
+
+//funcion de copiado en memoria
 function copy(){
     let texto = document.getElementById('texto__respuesta').innerHTML;
     
